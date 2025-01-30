011a2ce853bb973ad827ca95cecc254cdc870d7d
Dawit_D <dawitccnt@gmail.com>
2025-01-30 12:22:41 +0300

    modules

---
+++ modules/modules.py
@@ -0,0 +1,15 @@
+import math
+from math import pi
+import childModule
+from childModule import sayHello
+from modules import childModule
+
+# print(math.pi)
+# print(sayHello())
+# print(dir(math))
+
+# for item in dir(math):
+#     print(item)
+
+print(__name__)
+print(childModule.__name__)
